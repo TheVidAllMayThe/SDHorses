88e5153cc279de0167ae5b100acb7657c62bc536
Manuel Xarez <manuelxarez@ua.pt>
2018-03-08 15:02:09 +0000

    some things paddoc

--- src/Paddock.java
+++ src/Paddock.java
@@ -1,31 +1,116 @@
-import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Paddock implements Paddock_Broker{
     private final Lock r1;
-    private final Condition brokerWait;
-    private final Condition horsesWait;
-    private final Condition spectatorsWait;
-    private final ArrayList<HorseInPaddock> horses = new ArrayList<>();
+    private final Condition brokerCond;
+    private final Condition horsesEnter;
+    private final Condition horsesLeave;
+    private final Condition spectatorsEnter;
+    private final Condition spectatorsLeave;
+    private final HorseInPaddock horses[];
+    private final int nHorses;
+    private final int nSpectators;
 
-    Paddock(){
+    private Boolean allowHorsesEnter;
+    private Boolean allowHorsesLeave;
+    private Boolean allowSpectatorsEnter;
+    private Boolean allowSpectatorsLeave;
+    private int horsesInPaddock;
+    private int spectatorsInPaddock;
+
+    Paddock(int nHorses, int nSpectators){
         this.r1 = new ReentrantLock(false);
-        this.brokerWait = r1.newCondition();
-        this.horsesWait = r1.newCondition();
-        this.spectatorsWait = r1.newCondition();
+        this.horsesEnter = r1.newCondition();
+        this.horsesLeave = r1.newCondition();
+        this.brokerCond = r1.newCondition();
+        this.spectatorsEnter = r1.newCondition();
+        this.spectatorsLeave = r1.newCondition();
+        this.allowHorsesEnter = false;
+        this.allowHorsesLeave = false;
+        this.allowSpectatorsEnter = false;
+        this.allowSpectatorsLeave = false;
+        this.nHorses = nHorses;
+        this.nSpectators = nSpectators;
+        this.horsesInPaddock = 0;
+        this.spectatorsInPaddock = 0;
+        this.horses = new HorseInPaddock[nHorses];
     }
 
-    public void SummonHorsesToPaddock(){
-        
+    public void proceedToPaddock(int horseID, int pnk){
+        r1.lock();
+        try {
+            while (!allowHorsesEnter) {
+                horsesEnter.await();
+            }
+
+            this.horses[this.horsesInPaddock++] = new HorseInPaddock(horseID, pnk);
+
+            if(horsesInPaddock == nHorses){
+                this.allowSpectatorsEnter = true;
+                spectatorsEnter.signal();
+            }
+            else{
+                horsesEnter.signal();
+            }
+
+        }catch(InterruptedException ie){
+
+        }finally{
+            r1.unlock();
+        }
     }
 
-    private class HorseInPaddock{
+    public HorseInPaddock[] goCheckHorses(){
+        r1.lock();
+        try {
+            while (!allowSpectatorsEnter){
+                spectatorsEnter.await();
+            }
+
+            this.spectatorsInPaddock++;
+
+            if(spectatorsInPaddock == nSpectators){
+                allowHorsesLeave = true;
+                horsesLeave.signal();
+                brokerCond.signal();
+            }
+            else{
+                spectatorsEnter.signal();
+            }
+
+            return horses;
+
+        }catch(InterruptedException ie){
+
+        }finally{
+            r1.unlock();
+        }
+    }
+
+    public void proceedToStartLine(){
+        r1.lock();
+        try {
+            while (!allowHorsesLeave) {
+                horsesLeave.await();
+            }
+
+
+        }catch(InterruptedException ie){
+
+        }finally{
+            r1.unlock();
+        }
+    }
+
+    private class HorseInPaddock {
         public int horseID;
         public int pnk;
+
+        HorseInPaddock(int horseID, int pnk) {
+            this.horseID = horseID;
+            this.pnk = pnk;
+        }
     }
-
-
 }
-
